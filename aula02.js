6b2bc6f95b3cb1d050a07c12567bf2f7b2a244ef
Lara Luiz <lara.luiz.barros@escola.pr.gov.br>
2026-07-28 11:21:59 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,13 @@
+// CONDICIONAIS
+
+  let nota1 = 6;
+  let nota2 = 10;
+  let nota3 = 5;
+
+  mediaFinal = nota1 + nota2 +nota3;
+
+  if(mediafinal >6) {
+    console.log("Aluno aprovado! com nota:" + mediaFinal);
+  }else{
+     console.log("Aluno reprovado com nota:" + mediaFinal)
+  }
