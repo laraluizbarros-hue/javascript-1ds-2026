dee53be3a0e8cef580058375d916a147c2a6a208
Lara Luiz <lara.luiz.barros@escola.pr.gov.br>
2026-07-28 15:32:31 -0300

    Add aula01.js with variable declarations and logs

---
+++ aula01.js
@@ -0,0 +1,9 @@
+    // CRIANDO VARIÁVEIS PARA O SISTEMA
+
+    let nome = "Lara";
+    var idade = 16;
+    const altura = 1.60;
+
+    console.log( nome );
+    console.log( idade );
+    console.log( altura );
