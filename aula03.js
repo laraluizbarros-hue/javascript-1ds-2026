0728a9a5e408c3a36d5bff2cfe767810469dc154
Lara Luiz <lara.luiz.barros@escola.pr.gov.br>
2026-08-14 08:37:32 -0300

    Add files via upload

---
+++ aula03.js
@@ -0,0 +1,24 @@
+let nome = "Lara";
+
+if (nome== "Lara") {
+ console.log("Voce digitou o nome correto!")
+}else{
+    console.log("Voce digitou outra coisa . . . ")
+}
+
+  let idade = 18;
+   
+  if (idade == 18) {
+   console.log("Voce é praticamente um adulto . . .")
+  }else if(idade < 18){
+    console.log("Voce ainda é de menor . . .")
+ }else{
+    console.log("Voce é adulto . . . ")
+ }
+
+ //FAÇA UMA VERIFICAÇÃO COM IF E ELSE
+ // DE UMA PESSOA SE ELA PODE OU NÃO VOTAR
+    
+    
+
+ 
